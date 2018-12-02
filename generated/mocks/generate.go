9e3b4307d4a647fea870e179dc5cf8f2f2df505c
Xi Chen <xichen@uber.com>
2018-12-02 18:54:30 -0500

    More efficient encoding of full doc ID set

--- generated/mocks/generate.go
+++ generated/mocks/generate.go
@@ -1,7 +1,7 @@
 package mocks
 
 // mockgen rules for generating mocks for exported interfaces (reflection mode).
-//go:generate sh -c "mockgen -package=int $PACKAGE/encoding/int Encoder,Iterator,Decoder | genclean -pkg $PACKAGE/encoding/int -out $GOPATH/src/$PACKAGE/encoding/int/encoding_mock.go"
+//go:generate sh -c "mockgen -package=digest $PACKAGE/digest FdWithDigestWriter | genclean -pkg $PACKAGE/digest -out $GOPATH/src/$PACKAGE/digest/writer_mock.go"
 
 // mockgen rules for generating mocks for unexported interfaces (file mode).
 //go:generate sh -c "mockgen -package=storage -destination=$GOPATH/src/$PACKAGE/storage/segment_mock.go -source=$GOPATH/src/$PACKAGE/storage/segment.go"
