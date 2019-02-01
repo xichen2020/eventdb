9ec39a6e1407b3c02a4bc9a115caceec789e55e6
Won Jun Jang <wonjun.jangobe@gmail.com>
2019-02-01 13:21:32 -0500

    Add benchmark for roaring bit map snapshot (#97)

---
+++ index/doc_id_set_bench_test.go
@@ -0,0 +1,52 @@
+package index
+
+import (
+	"testing"
+
+	"github.com/pilosa/pilosa/roaring"
+)
+
+/*
+Summary: Snapshot of dense bitmap takes 45us. Gets cheaper the sparser the dataset.
+
+BenchmarkFullDocIDSetCloneSnapshot-8      	   30000	     44430 ns/op	  265336 B/op	      69 allocs/op
+BenchmarkDenseDocIDSetCloneSnapshot-8   	   30000	     48289 ns/op	  265336 B/op	      69 allocs/op
+BenchmarkSparseDocIDSetCloneSnapshot-8    	  500000	      4085 ns/op	   12408 B/op	      69 allocs/op
+BenchmarkDocIDSetCheapSnapshot-8          	2000000000	         0.29 ns/op	       0 B/op	       0 allocs/op
+*/
+
+func BenchmarkFullDocIDSetCloneSnapshot(b *testing.B) {
+	bm := initBenchBitmap(benchNumTotalDocs, 1)
+	benchmarkDocIDSetCloneSnapshot(b, bm)
+}
+
+func BenchmarkDenseDocIDSetCloneSnapshot(b *testing.B) {
+	bm := initBenchBitmap(benchNumTotalDocs, 3)
+	benchmarkDocIDSetCloneSnapshot(b, bm)
+}
+
+func BenchmarkSparseDocIDSetCloneSnapshot(b *testing.B) {
+	bm := initBenchBitmap(benchNumTotalDocs, 500)
+	benchmarkDocIDSetCloneSnapshot(b, bm)
+}
+
+func BenchmarkDocIDSetCheapSnapshot(b *testing.B) {
+	bm := initBenchBitmap(benchNumTotalDocs, 1)
+	benchmarkDocIDSetCheapSnapshot(b, bm)
+}
+
+func benchmarkDocIDSetCloneSnapshot(b *testing.B, bm *roaring.Bitmap) {
+	builder := NewBitmapBasedDocIDSetBuilder(bm)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_ = builder.Snapshot()
+	}
+}
+
+func benchmarkDocIDSetCheapSnapshot(b *testing.B, bm *roaring.Bitmap) {
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		// TODO(wjang): This is an approximation of the cloning we'll be doing.
+		_ = newBitmapBasedDocIDSet(bm)
+	}
+}
