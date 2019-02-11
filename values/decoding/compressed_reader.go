300f5d39a1ff0a55d0afa13ca373748b0134ab73
Bo Du <bokwandu@gmail.com>
2019-02-11 13:48:15 -0500

    Add some safeguards against double close.

--- values/decoding/compressed_reader.go
+++ values/decoding/compressed_reader.go
@@ -1,9 +1,14 @@
 package decoding
 
 import (
+	"errors"
 	"io"
 
 	"github.com/valyala/gozstd"
+)
+
+var (
+	errCompressedReaderClosed = errors.New("compressedReader is closed")
 )
 
 // compressedReader reads compressed data.
@@ -14,6 +19,8 @@ type compressedReader struct {
 	// we can read in a single byte at a time to
 	// impl the `io.ByteReader` iface.
 	buf [1]byte
+
+	closed bool
 }
 
 // newCompressedReader returns a new CompressReader instance.
@@ -25,6 +32,9 @@ func newCompressedReader(reader io.Reader) *compressedReader {
 
 // ReadByte read a byte.
 func (cr *compressedReader) ReadByte() (byte, error) {
+	if cr.closed {
+		return 0, errCompressedReaderClosed
+	}
 	if _, err := cr.Read(cr.buf[:]); err != nil {
 		return 0, err
 	}
@@ -33,6 +43,11 @@ func (cr *compressedReader) ReadByte() (byte, error) {
 
 // Close releases the underlying resources of the compressed reader.
 func (cr *compressedReader) Close() {
+	if cr.closed {
+		return
+	}
+
 	// Release does not return an error.
 	cr.Release()
+	cr.closed = true
 }
